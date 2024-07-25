4e7d896fc4f95a24e69bef837fe1662174b3e747
Kierrany <kierranyc@outlook.com>
2024-07-25 18:23:43 -0500

    fixing styling for contact and footer sections, wrote bio, gotta fix styling for project sections now

--- script.js
+++ script.js
@@ -4,3 +4,47 @@ function toggleMenu() {
   menu.classList.toggle('open')
   icon.classList.toggle('open')
 }
+
+document.querySelector('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const formData = new FormData(form);
+  const formMessages = document.querySelector('#form-messages');
+
+  fetch(form.action, {
+    method: form.method,
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      showModal('Thank you! Your message has been sent.');
+      form.reset();
+    } else {
+      return response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          showModal(data.errors.map(error => error.message).join(', '));
+        } else {
+          showModal('Oops! There was a problem submitting your form');
+        }
+      });
+    }
+  }).catch(error => {
+    showModal('Oops! There was a problem submitting your form');
+  });
+});
+
+function showModal(message) {
+  const modal = document.getElementById('modal');
+  const modalMessage = document.getElementById('modal-message');
+
+  modalMessage.textContent = message;
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+}
